4b974cabb3d3dfdac9696e56abbdbb0e286045eb
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2016-03-01 00:37:52 +0100

    Added pictures

--- app/stores/StateStore.jsx
+++ app/stores/StateStore.jsx
@@ -1,18 +1,45 @@
 import Reflux from 'reflux';
 import StateAction from '../actions/StateAction';
 
-import house from '../styles/cards/house.png';
-import hero from '../styles/cards/hero.png';
-import list from '../styles/cards/list.png';
-import boss from '../styles/cards/boss.png';
-import inventory from '../styles/cards/inventory.png';
-import spicyLand from '../styles/cards/chili_land.png';
-import spicyMonster from '../styles/cards/boss.png';
-import sweetLand from '../styles/cards/sweet_land.png';
-import sweetMonster from '../styles/cards/sweet_monster.png';
-import sourLand from '../styles/cards/sour_land.png';
-import sourMonster from '../styles/cards/sweet_monster.png';
-import tree from '../styles/cards/tree.jpg';
+import sova from '../styles/cards/sova.jpg';
+import storaTorget from '../styles/cards/stora_torget.jpg';
+import per from '../styles/cards/per.jpg';
+import train from '../styles/cards/train.jpg';
+import stockholm from '../styles/cards/stockholm.jpg';
+import gata from '../styles/cards/gata.jpg';
+import annikaEmilia from '../styles/cards/annika_emilia.jpg';
+import sister from '../styles/cards/sister.jpg';
+import universe from '../styles/cards/universe.jpg';
+import festivas from '../styles/cards/festivas.jpg';
+import vasby from '../styles/cards/vasby.jpg';
+import lars from '../styles/cards/lars.jpg';
+import buss from '../styles/cards/buss.png';
+import scary from '../styles/cards/scary.jpg';
+import skepptuna from '../styles/cards/skepptuna.png';
+import kaffe from '../styles/cards/kaffe.jpg';
+import explosion from '../styles/cards/explosion.jpg';
+import cape from '../styles/cards/cape.jpg';
+import trolls from '../styles/cards/trolls.jpg';
+import derp from '../styles/cards/derp.jpg';
+import imaginary from '../styles/cards/imaginary_train.jpg';
+import vasaloppet from '../styles/cards/vasaloppet.png';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
+//import tree from '../styles/cards/tree.jpg';
 
 const StateStore = Reflux.createStore({
 
@@ -25,22 +52,25 @@ const StateStore = Reflux.createStore({
       text: "Du vaknar upp med ett ryck, du ser dig omkring och drar slutsatsen att du är på ett snabbmatställe. " +
             "Men vem är du, och hur hamnade du här egentligen? Du känner dig väldigt förvirrad. Du hittar en tom " +
             "ryggsäck som ligger bredvid dig, den är förmodligen din, eftersom det inte är någon annan i närheten.",
-      img: tree,
+      img: sova,
       choices: [{text: "Gå ut", nextState: 2}, {text: "Fortsätt sov lite till", nextState: 1}]
     },
     {
       page: 2,
+      img: storaTorget,
       text: "Du kommer ut på ett torg med några busshållplatser.",
       choices: [{text: "Fråga någon vart du är", nextState: 3}, {text: "Gå i slumpmässig riktning", nextState: 4}]
     },
     {
-      page: 3, //Per
+      page: 3,
+      img: per,
       text: "Personen du frågar verkar känna igen dig och säger: \"Roligt skämt min gode herre, " +
             "vi är ju självfallet på stora torget i Uppsala\"",
       choices: [{text: "Fortsätt gå", nextState: 4}, {text: "Småprata med honom", nextState: 5}]
     },
     {
       page: 4,
+      img: train,
       text: "Du hamnar på Uppsala centralstation, kanske behöver du åka någonstans för att få reda på vem du är?",
       choices: [{text: "Stockholm", nextState: 6},
                 {text: "Västerås", nextState: 12},
@@ -53,6 +83,7 @@ const StateStore = Reflux.createStore({
     },
     {
       page: 5, //Per
+      img: per,
       text: "Per som personen hette, gör en lång utläggning om religion, meningen med livet och förklarar varför hundar " +
             "är bättre än katter. Du somnar i en hög på asfalten och får aldrig reda på vem du är.",
       choices: [{text: "Börja om", nextState: 1}]
@@ -60,11 +91,13 @@ const StateStore = Reflux.createStore({
     // Stockholm
     {
       page: 6,
+      img: stockholm,
       text: "Tåget anländer två timmar för sent till centralstationen i Stockholm",
       choices: [{text: "Klaga till biljettkontrollanten!", nextState: 7}, {text: "Gå till plattan.", nextState: 8}]
     },
     {
       page: 7, // Någon vän photoshoppad som kontrollant
+      img: sj,
       item: "SJ-bälte",
       text: "Kontrollanten ber så hemskt mycket om ursäkt och ger dig sitt " +
             "bälte som det står SJ i stora bokstäver på som kompensation.",
@@ -72,17 +105,20 @@ const StateStore = Reflux.createStore({
     },
     {
       page: 8, // Bild på vägen till plattan
+      img: gata,
       text: "\"Heeeej Jens!\" är det någon som skriker, det verkar vara riktat till dig.",
       choices: [{text: "Undersök saken", nextState: 9}, {text: "Lunka vidare", nextState: 10}]
     },
     {
-      page: 9, // Annika och
+      page: 9, // Annika och Emilia
+      img: annikaEmilia,
       text: "Två tjejer kramar om dig, men säger att de har väldigt bråttom till sitt tåg, så de springer snabbt iväg. " +
             "Nu vet du ditt namn i alla fall, alltid nått positivt!",
       choices: [{text: "Gå vidare", nextState: 10}, {text: "Få existenskris", nextState: 1}]
     },
     {
       page: 10,
+      img: sister,
       item: "Coola skor",
       text: "\"Vad gör du här bror? Åk hem till Uppsala, de letar efter dig där! " +
             "Du glömde dina springskor hos mig i förrgår förresten. Här.\" Malin ger dig dina skor.",
@@ -90,6 +126,7 @@ const StateStore = Reflux.createStore({
     },
     {
       page: 11,
+      img: universe,
       text: "Du tar på dig springskorna och automagiskt så börjar du springa. Problemet är bara att du inte kan stanna, " +
             "du behöver nått som ger lite luftmotstånd! Du springer till universums undergång, " +
             "trevligt men kontraproduktivt.",
@@ -98,23 +135,27 @@ const StateStore = Reflux.createStore({
     // Västerås
     {
       page: 12,
+      img: festivas,
       text: "Du hamnar i en studentlägenhet och någon säger att det är Festivås. Efter några timmar somnar du till.",
       choices: [{text: "Vakna!", nextState: 1}]
     },
     // Upplands väsby
     {
       page: 13,
+      img: vasby,
       text: "Tåget rullar in på stationen. Det här känns bekant på nått sätt...",
       choices: [{text: "Hoppa på en buss", nextState: 16}, {text: "Låt fötterna leda dig", nextState: 14}]
     },
     {
       page: 14,
+      img: skepptuna,
       text: "Du kommer fram till ett hus som känns väldigt bekant. " +
             "Det står någon i fönstet som vinkar att du ska komma in.",
       choices: [{text: "Creepy, spring därifrån!", nextState: 11}, {text: "Gå in.", nextState: 15}]
     },
     {
       page: 15,
+      img: lars,
       item: "Spandex brallor",
       text: "Det är tydligen din Pappa. \"Tur att man hade en sånn i alla fall\", tänker du. " +
       "Han bjuder dig på lunch och efterrätt. Efter lunchen ger han dig ett par tighta spandex-cykelbrallor som han tror " +
@@ -123,6 +164,7 @@ const StateStore = Reflux.createStore({
     },
     {
       page: 16,
+      img: buss,
       text: "Du står vid busshållplatsen, vart vill du ta dig?",
       choices: [{text: "Uppsala", nextState: 4},
                 {text: "Vattmåravägen", nextState: 17},
@@ -130,18 +172,21 @@ const StateStore = Reflux.createStore({
                 {text: "Smedby", nextState: 22}]
     },
     {
-      page: 17, // Läskig bild på Emelie, Jossan och Malin
+      page: 17,
+      img: scary,
       text: "Det står några tjejer där, de ser fruktansvärt läskiga ut",
       choices: [{text: "Gå baklänges till busshållplatsen", nextState: 16}, {text: "Prata med dem", nextState: 18}]
     },
     {
       page: 18, // Kaffe
+      img: kaffe,
       text: "De bjuder in dig på kaffe. Du hatar kaffe och det gör alla dem också. De berättar om någon superhjälte som " +
             "varit i farten den senaste tiden, men som nu spårlöst är försvunnen.",
       choices: [{text: "Du flyr till busshållplatsen", nextState: 16}, {text: "Ta en till kopp kaffe", nextState: 19}]
     },
     {
       page: 19, // Explosion
+      img: explosion,
       text: "Du tål inte så här mycket koffein. Du exploderar. Jahapp, det var ju oturligt.",
       choices: [{text: "Börja om", nextState: 1}]
     },
@@ -152,32 +197,36 @@ const StateStore = Reflux.createStore({
     },
     {
       page: 21, // Bild på hästtäcke
+      img: cape,
       item: "Mantel",
       text: "De känner igen dig. Väldigt konstigt att varenda person du träffar på känner och verkar gilla dig, tänker du." +
       "Vad är det här egentligen, Truman Show?! De kramar om dig och ger dig ett hästtäcke som nästan ser ut som en mantel.",
       choices: [{text: "Tacka artigt och gå till bussen", nextState: 16}]
     },
     {
-      page: 22, // Johan och petter med tophats
+      page: 22, // Johan och Nibbe med tophats
+      img: trolls,
       text: "Du träffar på två gentlemän. De tycker det är fruktansvärt roligt att du inte kommer ihåg nått. " +
             "Snart märker du att det inte alls var gentlemän, det var internettroll! " +
             "De kastar bort all tid du har utan att ge nått vettigt tillbaka.",
-      choices: [{text: "Spola tillbaka tiden", nextState: 1}]
+      choices: [{text: "Spola tillbaka tiden", nextState: 23}]
     },
     {
       page: 23,
-      text: "De bjuder in dig på kaffe. Du hatar kaffe och det gör alla dem också. De berättar om någon superhjälte som " +
-            "varit i farten den senaste tiden, men som nu spårlöst är försvunnen.",
-      choices: [{text: "Du flyr till busshållplatsen", nextState: 16}, {text: "Ta en till kopp kaffe", nextState: 17}]
+      img: derp,
+      text: "DEEERP!",
+      choices: [{text: "Spola snabbare!!!", nextState: 1}]
     },
     // Sälen
     {
       page: 24,
+      img: imaginary,
       text: "Du kommer till Sälens tågstation, ypperligt fin. Eller nått. Särskilt eftersom den inte finns på riktigt.",
       choices: [{text: "Kontemplera över verkligheten", nextState: 25}, {text: "Åk lite mer imaginärt tåg!", nextState: 4}]
     },
     {
       page: 25,
+      img: vasaloppet,
       text: "Under kontemplererandet så har du händerna i fickorna, så som sig bör. I fickan hittar du ett startnummer till " +
       "Vasaloppet. Vad kan gå fel tänker du.",
       choices: [{text: "Ta dig till starten", nextState: 26}, {text: "Ugh, jobbigt, roligare med tåg!", nextState: 4}]
